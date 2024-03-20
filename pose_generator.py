c933405f05d2af16dfd2e375b1c6814784229240
Juan Antonio Barragan <jbarrag3@jh.edu>
2024-03-20 19:56:52 -0400

    Reformat files with black

--- pose_generator.py
+++ pose_generator.py
@@ -18,6 +18,7 @@ def convex_hull(points):
         hull_points = points_array[hull.vertices]
         return (hull, scipy.spatial.Delaunay(hull_points))
 
+
 def intersection(hull, start, ray):
     hull, _ = hull
     normals = hull.equations[:, 0:-1]
@@ -30,13 +31,15 @@ def intersection(hull, start, ray):
 
     alpha = np.min(A[A > 0])
 
-    return 0.995*alpha
+    return 0.995 * alpha
+
 
 def in_hull(hull, pose):
     point = pose[0:3]
     _, triangulation = hull
     result = triangulation.find_simplex([point]) >= 0
     return result[0]
+
 
 def centroid(hull):
     hull, delaunay = hull
@@ -53,19 +56,20 @@ def centroid(hull):
 
     return centroid / total_volume
 
+
 def display_hull(hull):
     def plot_hull(ax, hull):
         hull, _ = hull
         ax.plot(hull.points.T[0], hull.points.T[1], hull.points.T[2], "ko")
 
         for s in hull.simplices:
-            s = np.append(s, s[0]) # close cycle
+            s = np.append(s, s[0])  # close cycle
             ax.plot(hull.points[s, 0], hull.points[s, 1], hull.points[s, 2], "r-")
 
     fig = plt.figure("Safe ROM Display")
     ax = fig.add_subplot(projection="3d")
-    limits = np.array([getattr(ax, f'get_{axis}lim')() for axis in 'xyz'])
-    ax.auto_scale_xyz(*[[np.min(limits), np.max(limits)]]*3)
+    limits = np.array([getattr(ax, f"get_{axis}lim")() for axis in "xyz"])
+    ax.auto_scale_xyz(*[[np.min(limits), np.max(limits)]] * 3)
     plot_hull(ax, hull)
     ax.set_xlabel("x")
     ax.set_ylabel("y")
